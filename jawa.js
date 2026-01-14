db51778cc102d607f19f26e50e928036c9b53e0d
Elaangg <elang_syadewa_34rpl@student.smktelkom-mlg.sch.id>
2026-01-14 09:21:33 +0700

    Add files via upload

---
+++ jawa.js
@@ -0,0 +1,8 @@
+// Assigment 1 Declare Variable
+let name = "Elang Dimas Syadewa";
+let namapanggilan = "Elang";
+let age = "16";
+let isStudent = true;
+document.getElementById("ohWhoIsHe").innerHTML = name;
+document.getElementById("myAge").innerHTML = age;
+document.getElementById("studentStatus").innerHTML = isStudent;
